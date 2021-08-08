1fe9dd0fe8e119d5b9b19da3b166d6d2a69a15a8
heloowird <heloowird@gmail.com>
2021-08-08 19:30:38 +0800

    add shader class

---
+++ include/util/shader.h
@@ -0,0 +1,111 @@
+#ifndef OPENGL_EXAMPLE_SHADER_H
+#define OPENGL_EXAMPLE_SHADER_H
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+#include <glad/glad.h>
+
+class Shader
+{
+public:
+    // 着色器程序ID
+    unsigned int ID;
+
+    // 从文件读取着色器源码, 构建并编译着色程序
+    Shader(const GLchar* vertexPath, const GLchar* fragmentPath) {
+        std::string vertexCode;
+        std::string fragmentCode;
+        std::ifstream vShaderFile;
+        std::ifstream fShaderFile;
+        try {
+            // 打开文件
+            vShaderFile.open(vertexPath);
+            fShaderFile.open(fragmentPath);
+            std::stringstream vShaderStream, fShaderStream;
+            // 读取文件的缓冲内容到数据流中
+            vShaderStream << vShaderFile.rdbuf();
+            fShaderStream << fShaderFile.rdbuf();
+            // 关闭文件处理器
+            vShaderFile.close();
+            fShaderFile.close();
+            // 转换数据流到string
+            vertexCode   = vShaderStream.str();
+            fragmentCode = fShaderStream.str();
+        } catch (...){
+            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+        }
+
+        const char* vShaderCode = vertexCode.c_str();
+        const char* fShaderCode = fragmentCode.c_str();
+
+        // 编译着色器
+        unsigned int vertex, fragment;
+
+        // 创建和编译顶点着色器
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+        checkShaderCompile(vertex, "VERTEX");
+
+        // 创建和编译片段着色器
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+        checkShaderCompile(fragment, "FRAGMENT");
+
+        ID = glCreateProgram();
+        // 把之前编译的着色器附加到程序对象上
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glLinkProgram(ID);
+        // 检查着色程序编译是否成功
+        int success;
+        char infoLog[512];
+        glGetProgramiv(ID, GL_LINK_STATUS, &success);
+        if(!success) {
+            glGetProgramInfoLog(ID, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        }
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+    }
+
+    // 使用/激活程序
+    void use() {
+        glUseProgram(ID);
+    }
+
+    // 删除着色程序
+    void del() {
+        glDeleteProgram(ID);
+    }
+
+    // uniform工具函数
+    void setBool(const std::string &name, bool value) const {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+    }
+
+    void setInt(const std::string &name, int value) const {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+
+    void setFloat(const std::string &name, float value) const {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
+
+private:
+    void checkShaderCompile(const unsigned int &shaderID, const std::string &shaderType) {
+        int success;
+        char infoLog[512];
+        glGetShaderiv(shaderID, GL_COMPILE_STATUS, &success);
+        if(!success)
+        {
+            glGetShaderInfoLog(shaderID, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::" << shaderType << "::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+    }
+};
+#endif //OPENGL_EXAMPLE_SHADER_H
