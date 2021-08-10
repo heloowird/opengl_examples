4d39b27d5d64f246716e55467af9015c218c33f7
heloowird <heloowird@gmail.com>
2021-08-10 22:04:36 +0800

    add exercise1 answer

--- src/1_getting_started/4_3_texture_mix_texture.fs
+++ src/1_getting_started/4_3_texture_mix_texture.fs
@@ -10,4 +10,5 @@ uniform sampler2D texture2;
 void main() {
     // 两个纹理线性插值混合 (80% texture1，20% texture2)
     FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    //FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(1.0 - TexCoord.x, TexCoord.y)), 0.2); // exercise1
 }
