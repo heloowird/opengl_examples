a6a7d711f3cd2c17116db39f67e3eb48b70cbafb
heloowird <heloowird@gmail.com>
2021-08-10 08:44:01 +0800

    add mix textures

---
+++ src/1_getting_started/4_3_texture_mix_texture.fs
@@ -0,0 +1,13 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main() {
+    // 两个纹理线性插值混合 (80% texture1，20% texture2)
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+}
